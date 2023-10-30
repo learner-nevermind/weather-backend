685cb501a1450c46a0eb0f44af6720656df8a9cd
learner-nevermind <superdevv0292@gmail.com>
2023-10-30 01:30:15 -0400

    updated

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
+// const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 // const routes = require("./routes");
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 // app.use(routes);
 
-require("dotenv").config();
+// require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
+// mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
 
 // app.listen(process.env.SERVER_PORT, () => {
 //   console.log(`The server started on ${process.env.SERVER_PORT}`);
