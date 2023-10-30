ed1b4f20d336da0b15150006eefb15fb049657a1
learner-nevermind <superdevv0292@gmail.com>
2023-10-30 02:09:26 -0400

    ewrewrewr

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 // const routes = require("./routes");
@@ -12,7 +12,7 @@ app.use(cors());
 
 require("dotenv").config();
 
-// mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
 
 // app.listen(process.env.SERVER_PORT, () => {
 //   console.log(`The server started on ${process.env.SERVER_PORT}`);
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 const SALT_WORK_FACTOR = 10;
@@ -14,20 +14,20 @@ const User = new Schema({
   },
 });
 
-User.pre("save", function (next) {
-  var user = this;
+// User.pre("save", function (next) {
+//   var user = this;
 
-  if (!user.isModified("password")) return next();
+//   if (!user.isModified("password")) return next();
 
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) return next(err);
+//   bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+//     if (err) return next(err);
 
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
-});
+//     bcrypt.hash(user.password, salt, function (err, hash) {
+//       if (err) return next(err);
+//       user.password = hash;
+//       next();
+//     });
+//   });
+// });
 
 module.exports = mongoose.model("User", User);
