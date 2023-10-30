d2196610dd0405c6e02458804e303d5a5f22d1e3
learner-nevermind <superdevv0292@gmail.com>
2023-10-30 01:26:38 -0400

    updated

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const routes = require("./routes");
+// const routes = require("./routes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(routes);
+// app.use(routes);
 
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`The server started on ${process.env.SERVER_PORT}`);
-});
+// app.listen(process.env.SERVER_PORT, () => {
+//   console.log(`The server started on ${process.env.SERVER_PORT}`);
+// });
+
+
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
