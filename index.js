570075c972b81b13bb9ebf97468a6fd4bdd8c887
learner-nevermind <superdevv0292@gmail.com>
2023-10-30 01:08:29 -0400

    fix: exported app

--- index.js
+++ index.js
@@ -17,3 +17,5 @@ mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`The server started on ${process.env.SERVER_PORT}`);
 });
+
+module.exports = app
