68dbbe0c1e07e060254de7fe32a13e49d26de2f5
learner-nevermind <superdevv0292@gmail.com>
2023-10-30 10:28:23 -0400

    fix: updated index.js

--- index.js
+++ index.js
@@ -3,33 +3,19 @@ const app = express();
 const { connect } = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const routes = require("./routes");
+const routes = require("./routes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-// app.use(routes);
+app.use(routes);
 
 require("dotenv").config();
 
 connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
 
-// app.listen(process.env.SERVER_PORT, () => {
-//   console.log(`The server started on ${process.env.SERVER_PORT}`);
-// });
-
-const PORT = 4000;
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
-});
-
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`The server started on ${process.env.SERVER_PORT}`);
 });
 
 // Export the Express API
