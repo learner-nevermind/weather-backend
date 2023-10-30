ea4e1fd64fa0331203a53beb9a788bcfc87ae8c5
learner-nevermind <superdevv0292@gmail.com>
2023-10-30 01:27:47 -0400

    feat: updated

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
 // const routes = require("./routes");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cors());
 // app.use(routes);
 
-require("dotenv").config();
+// require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
+// mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
 
 // app.listen(process.env.SERVER_PORT, () => {
 //   console.log(`The server started on ${process.env.SERVER_PORT}`);
 // });
 
-
-const PORT = 4000
+const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT} `);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
+
+// Export the Express API
+module.exports = app;
