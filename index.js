fb6ef93463385a583ffeca56d25eaa6fb9309016
learner-nevermind <superdevv0292@gmail.com>
2023-10-30 01:28:36 -0400

    feat: updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const bodyParser = require("body-parser");
 // const cors = require("cors");
 // const routes = require("./routes");
@@ -10,9 +10,9 @@ const app = express();
 // app.use(cors());
 // app.use(routes);
 
-// require("dotenv").config();
+require("dotenv").config();
 
-// mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected!"));
 
 // app.listen(process.env.SERVER_PORT, () => {
 //   console.log(`The server started on ${process.env.SERVER_PORT}`);
